832340e31bfd53667ec320b84a5fbca52485b233
tczsoft <vinayagam.mgb@gmail.com>
2024-11-08 12:09:24 +0530

    08-11-2024

--- src/Shared/Services/api/apiendpoint.js
+++ src/Shared/Services/api/apiendpoint.js
@@ -1,6 +1,6 @@
 const apiurl=()=>{
     // var url="http://192.168.0.130:3000";
-    var url="http://15.207.38.60:3000";
+    var url="https://api.mykozan.com";
  return url;
 }
 export default apiurl;
